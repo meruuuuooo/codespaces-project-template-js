f0e3607890deeb74b948eeab12c8ceea72a3f148
meruuuuooo <merudeibitto@gmail.com>
2024-02-08 17:26:43 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mel David Mailem",
+  title: "Programmer",
+  email: "mailem.meldavid@gmail.com",
+  gitHub: "meruuuuooo",
+  instagram: "meruuuuooo",
+  linkedIn: "mel-david-mailem-5035a32b0",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mailem_meru",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/tudor-baciu-vc3iVL_znJ8-unsplash.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Simple Employee Management System",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+          "A simple employee management system using Java.",
+    url: "https://github.com/meruuuuooo/EmployeeManagementSystem",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Simple Project Management System",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      "A simple project management system using Java.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "My Resume Site",
+    title: "Flirtopia",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+        "A fun and charming Android app to generate flirty messages!",
+    url: "https://github.com/meruuuuooo/Flirtopia",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Beshiefy - Transform and Copy Text with Fun Emoji Characters!",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Beshiefy is a delightful web application that allows you to transform your input text into a creative representation using fun emoji characters. The app also provides a quick and easy way to copy the transformed text to your clipboard, ready to be pasted anywhere you like!",
+    url: "https://github.com/meruuuuooo/Beshiefy",
   },
 ];
 
